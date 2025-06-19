6837d850b7f57bade2edc3e4eacc8be38d6a17df
Soundar Balaji.J <75534427+devspidr@users.noreply.github.com>
2025-06-19 19:47:56 +0530

    Update 1910RemoveAllOccurencesofSubstring.java

--- 1910RemoveAllOccurencesofSubstring.java
+++ 1910RemoveAllOccurencesofSubstring.java
@@ -39,3 +39,22 @@ Now s has no occurrences of "xy".
         return s;
     }
 }
+
+class Solution {
+    public String removeOccurrences(String s, String part) {
+        StringBuilder stack = new StringBuilder();
+        
+        for (char ch : s.toCharArray()) {
+            stack.append(ch);
+            
+            if (stack.length() >= part.length()) {
+                if (stack.substring(stack.length() - part.length()).equals(part)) {
+                    stack.delete(stack.length() - part.length(), stack.length());
+                }
+            }
+        }
+        
+        return stack.toString();
+    }
+}
+
